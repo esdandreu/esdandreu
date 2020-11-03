ed09a678e58b49e1cf0869d11e55b8dbd7a5abe2
esdandreu <esdandreu@gmail.com>
2020-11-03 09:52:02 +0000

    WIP

---
+++ latexdocker.cmd
@@ -0,0 +1,4 @@
+:; echo "Running on *nix";  docker run -i --rm -w /data -v "$(pwd):/data" danteev/texlive "$*"; exit $?
+@ECHO OFF
+ECHO Running on Windows
+docker run --platform linux -i --rm -w /data -v "%cd%:/data" danteev/texlive %*
